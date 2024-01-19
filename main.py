0910c1e6ef83bc0ced8c5b91bf3eac0adefb5976
gustavolace <lsbgustavo@gmail.com>
2024-01-18 21:42:55 -0300

    direct links

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ options.add_argument("--user-data-dir=C:\\Users\\Gustavo Lacerda\\AppData\\Local
 
 driver = webdriver.Chrome(options=options)
 
-""" var = ["https://api.whatsapp.com/send?phone=5571981854906&text=iai%0Acaara", 
-       "https://api.whatsapp.com/send?phone=5571981854906&text=oi%0Atudo"] """
-
 var = [
     "https://web.whatsapp.com/send/?phone=5571981854906&text=oi%250tudo",
     "https://web.whatsapp.com/send/?phone=5571981854906&text=iai%0Acaara"
@@ -23,28 +20,6 @@ def teste(var):
     driver.switch_to.window(driver.window_handles[1])
     
     driver.get(f"{var}")
-
-    """ try:
-        action_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "action-button")))
-        print("Elemento 'action-button' encontrado")
-        # Encontrando o elemento span dentro do elemento 'action-button'
-        span = action_button.find_element(By.CLASS_NAME, '_advp')
-        span.click()
-    except Exception:
-        print("Erro ao encontrar elemento 'action-button':")
-
-    try:
-        use_whatsapp_web = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='_advp _aeam' and text()='use WhatsApp Web']")))
-        print("Elemento 'use WhatsApp Web' encontrado")
-
-        # Encontrando o link dentro do elemento 'use_whatsapp_web'
-        link_element = use_whatsapp_web.find_element(By.XPATH, "./ancestor::a")
-        link = link_element.get_attribute("href")
-
-        # Abre o link na página atual
-        driver.get(link)
-    except Exception as e:
-        print(f"Erro ao encontrar elemento 'use WhatsApp Web': {e}") """
 
     # Aguarda até 10 segundos para a presença do elemento com o seletor CSS "span[data-icon='send']" (ajuste conforme necessário)
     try:
