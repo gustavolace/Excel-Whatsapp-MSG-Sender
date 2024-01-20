28a3af297c8a324bfb78a3ca69eb973368ab6aa3
gustavolace <lsbgustavo@gmail.com>
2024-01-19 23:12:24 -0300

    exel text box Dispatch and row contacts

--- main.py
+++ main.py
@@ -2,34 +2,74 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pandas as pd
+import psutil
+from win32com.client import Dispatch
+from urllib.parse import quote
 import time
 
+var = []
+
+def is_excel_running():
+    for process in psutil.process_iter(['pid', 'name']):
+        if 'excel.exe' in process.info['name'].lower():
+            return True
+    return False
+num = is_excel_running()
+
+def find_text():
+    exel_status = is_excel_running()
+    xl = Dispatch('Excel.Application')
+    wb = xl.Workbooks.Open(Filename=r'C:\Users\Gustavo Lacerda\Downloads\vba.xlsm', )
+    ws = wb.Worksheets(1)
+    shapes = ws.Shapes(2)
+    text =  shapes.TextFrame.Characters().Text
+    if exel_status is False:
+        xl.Quit()
+
+    print("Text Box Text:", text)
+    return text
+
+def find_contacts():
+    # Ler o arquivo Excel
+    df = pd.read_excel(r'C:\Users\Gustavo Lacerda\Downloads\vba.xlsm', sheet_name='Planilha1', header=0, names=['Nome', 'Telefone'])
+
+    # Capturar dados da coluna A (nomes) e coluna B (telefones) apenas se as células não estiverem vazias
+    nomes = df['Nome'].dropna().tolist()
+    telefones = df['Telefone'].dropna().tolist()
+
+    # Exibir os dados capturados
+    return zip(nomes, telefones)
+
+def make_url():
+    contacts  = find_contacts()
+    text = find_text()
+    for nome, telefone in contacts :
+        client_text = text.replace("$cl", nome)
+        url = f"https://web.whatsapp.com/send/?phone={telefone}&text={quote(client_text)}"
+        var.append(url)
+        print(url)
+
+
+make_url()
+
 options = webdriver.ChromeOptions()
-#options.add_argument("--headless")
+options.add_argument("--headless")
 options.add_argument("--user-data-dir=C:\\Users\\Gustavo Lacerda\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 3")
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+options.add_argument(f'user-agent={user_agent}')
 
 driver = webdriver.Chrome(options=options)
 
-var = [
-    "https://web.whatsapp.com/send/?phone=5571981854906&text=oi%250tudo",
-    "https://web.whatsapp.com/send/?phone=5571981854906&text=iai%0Acaara"
-]
-
 def teste(var):
-    driver.execute_script("window.open('', '_blank');")
-    driver.switch_to.window(driver.window_handles[1])
-    
     driver.get(f"{var}")
 
-    # Aguarda até 10 segundos para a presença do elemento com o seletor CSS "span[data-icon='send']" (ajuste conforme necessário)
     try:
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[data-icon='send']")))
+        element = WebDriverWait(driver, 15).until(EC.presence_of_element_located((By.CSS_SELECTOR, "span[data-icon='send']")))
         print("Elemento span[data-icon='send'] encontrado")
         element.click()
 
         time.sleep(2)
-        driver.close()
-        driver.switch_to.window(driver.window_handles[0])
     except Exception:
         print("Erro ao encontrar elemento span[data-icon='send']:")
 
